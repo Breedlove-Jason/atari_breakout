39973837adc56654b995133d833cae2fc9b4d88e
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-28 13:41:02 -0700

    fix spacing and layout of bricks

--- bricks.py
+++ bricks.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -8,7 +9,8 @@ BRICK_ROWS = 8
 BRICK_COLS = 10
 BRICK_START_X = -452
 BRICK_START_Y = 290
-BRICK_COLORS = ["#FF00FF", "#00FFFF", "#FFFF00", "#FF8C00", "#FF1493", "#00FF00", "#FF4500", "#00FF7F"]
+BRICK_COLORS = ["#FF00FF", "#00FFFF", "#FFFF00", "#FF8C00", "#FF1493", "#00FF00", "#FF4500", "#1E90FF"]
+BRICK_Y_OFFSET = -26
 
 
 class Bricks:
@@ -21,26 +23,27 @@ class Bricks:
         brick.color(color)
         brick.shape("square")
         # brick.shapesize(stretch_wid=1, stretch_len=5)
-        brick.shapesize(stretch_wid=1, stretch_len=4)
+        brick.shapesize(stretch_wid=1, stretch_len=5)
         brick.penup()
         brick.goto(x, y)
         brick.stamp()
+        stamp_id = brick.stamp()
+        brick.clear()
 
         border = Turtle()
         border.color("black")
         border.shape("square")
-        border.shapesize(stretch_wid=1.1, stretch_len=4.1)
+        border.shapesize(stretch_wid=1.1, stretch_len=5.4)
         border.penup()
         border.goto(x, y)
         border.stamp()
 
         stamp_id = brick.stamp()
-        brick.clear()
         self.bricks.append({'x': x, 'y': y, 'color': color, 'stamp_id': stamp_id, 'border': border})
 
     def create_bricks(self):
         for row in range(BRICK_ROWS):
-            y = BRICK_START_Y - row * BRICK_HEIGHT
+            y = BRICK_START_Y - row * (BRICK_HEIGHT + BRICK_Y_OFFSET)
             color = BRICK_COLORS[row % len(BRICK_COLORS)]
             for x in range(BRICK_START_X, BRICK_START_X + int(BRICK_COLS * BRICK_WIDTH), int(BRICK_WIDTH)):
                 self.create_single_brick(x, y, color)
